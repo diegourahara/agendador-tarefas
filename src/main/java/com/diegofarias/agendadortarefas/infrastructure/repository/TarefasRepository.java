6963207fe23b7bb1e11840aa666f2381a821f057
Diego <dabedias@gmail.com>
2025-12-17 18:53:03 -0300

    adicionado dockerfile e alterações necessárias

--- src/main/java/com/diegofarias/agendadortarefas/infrastructure/repository/TarefasRepository.java
+++ src/main/java/com/diegofarias/agendadortarefas/infrastructure/repository/TarefasRepository.java
@@ -1,6 +1,7 @@
 package com.diegofarias.agendadortarefas.infrastructure.repository;
 
 import com.diegofarias.agendadortarefas.infrastructure.entity.TarefasEntity;
+import com.diegofarias.agendadortarefas.infrastructure.enums.StatusNotificacaoEnum;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
@@ -10,7 +11,11 @@ import java.util.List;
 @Repository
 public interface TarefasRepository extends MongoRepository<TarefasEntity, String> {
 
-    List<TarefasEntity> findByDataEventoBetween(LocalDateTime dataInicial, LocalDateTime dataFinal);
+    List<TarefasEntity> findByDataEventoBetweenAndStatusNotificacaoEnum(
+            LocalDateTime dataInicial,
+            LocalDateTime dataFinal,
+            StatusNotificacaoEnum statusNotificacaoEnum
+    );
 
     List<TarefasEntity> findByEmailUsuario(String email);
 
